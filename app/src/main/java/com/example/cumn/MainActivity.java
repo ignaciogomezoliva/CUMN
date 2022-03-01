161e12c47a015e109a888c7a3cf4bc4cbe07ae64
FernandoPeraltaAlba <fernando.peralta.alba@alumnos.upm.es>
2022-03-01 20:08:55 +0100

    Comentario en MainActivity

--- app/src/main/java/com/example/cumn/MainActivity.java
+++ app/src/main/java/com/example/cumn/MainActivity.java
@@ -47,5 +47,6 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         return NavigationUI.navigateUp(navController, appBarConfiguration)
                 || super.onSupportNavigateUp();
+        // HOla
     }
 }
