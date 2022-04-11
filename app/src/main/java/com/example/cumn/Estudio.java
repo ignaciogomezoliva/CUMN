ef4ecccc319684e692842c94a1b1dd9973f8acf6
FernandoPeraltaAlba <fernando.peralta.alba@alumnos.upm.es>
2022-04-11 21:00:25 +0200

    RecyclerViewAdapter added with the layouts

--- app/src/main/java/com/example/cumn/Estudio.java
+++ app/src/main/java/com/example/cumn/Estudio.java
@@ -1,14 +1,73 @@
 package com.example.cumn;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class Estudio extends AppCompatActivity {
+
+    private ArrayList<String> preguntas = new ArrayList<>();
+    private ArrayList<String> respuestas = new ArrayList<>();
+
+    private SharedPreferences preferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_estudio);
+
+        initPreguntas();
+    }
+
+    private void initPreguntas(){
+        //De prueba
+        preguntas.add("¿Cómo estas?");
+        respuestas.add("Bien");
+        preguntas.add("¿Cómo estas?");
+        respuestas.add("Bien1");
+        preguntas.add("¿Cómo estas?");
+        respuestas.add("Bien");
+        preguntas.add("¿Cómo estas?");
+        respuestas.add("Bien");
+        preguntas.add("¿Cómo estas?");
+        respuestas.add("Bien");
+        preguntas.add("¿Cómo estas?");
+        respuestas.add("Bien");
+        preguntas.add("¿Cómo estas?");
+        respuestas.add("Bien");
+        preguntas.add("¿Cómo estas?");
+        respuestas.add("Bien");
+        preguntas.add("¿Cómo estas?");
+        respuestas.add("Bien");
+        preguntas.add("¿Cómo estas?");
+        respuestas.add("Bien");
+        preguntas.add("¿Cómo estas?");
+        respuestas.add("Bien");
+        preguntas.add("¿Cómo estas?");
+        respuestas.add("Bien");
+        preguntas.add("¿Cómo estas?");
+        respuestas.add("Bien");
+        preguntas.add("¿Cómo estas?");
+        respuestas.add("Bien");
+        preguntas.add("¿Cómo estas?");
+        respuestas.add("Bien");
+        preguntas.add("¿Cómo estas?");
+        respuestas.add("Bien");
+        preguntas.add("¿Cómo estas?");
+        respuestas.add("Bien");
+
+        initRecyclerView();
+    }
+
+    private void initRecyclerView(){
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(preguntas,respuestas);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
---
+++ app/src/main/java/com/example/cumn/RecyclerViewAdapter.java
@@ -0,0 +1,58 @@
+package com.example.cumn;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>{
+
+    private ArrayList<String> preguntas = new ArrayList<>();
+    private ArrayList<String> respuestas = new ArrayList<>();
+    
+
+    public RecyclerViewAdapter(ArrayList<String> preguntas, ArrayList<String> respuestas) {
+        this.preguntas = preguntas;
+        this.respuestas = respuestas;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_listitem,parent,false);
+        ViewHolder holder = new ViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+       holder.pregunta.setText(preguntas.get(position));
+       holder.respuesta.setText(respuestas.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return preguntas.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        TextView pregunta;
+        TextView respuesta;
+
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            pregunta = itemView.findViewById(R.id.preguntaR);
+            respuesta = itemView.findViewById(R.id.respuestaR);
+            
+        }
+    }
+}
