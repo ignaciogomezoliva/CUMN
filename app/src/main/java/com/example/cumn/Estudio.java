f653f8ff5e2abe7046bfdd7bcf0af509b5f36610
FernandoPeraltaAlba <fernando.peralta.alba@alumnos.upm.es>
2022-04-12 02:32:32 +0200

    Preguntas and respuestas from API added

--- app/src/main/java/com/example/cumn/Estudio.java
+++ app/src/main/java/com/example/cumn/Estudio.java
@@ -4,15 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.example.cumn.rest.ServiceAPi;
+import com.example.cumn.rest.models.Pregunta;
+import com.example.cumn.rest.models.Result;
+
+import org.jsoup.Jsoup;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class Estudio extends AppCompatActivity {
 
     private ArrayList<String> preguntas = new ArrayList<>();
-    private ArrayList<String> respuestas = new ArrayList<>();
+    private ArrayList<String> respuestasR = new ArrayList<>();
 
     private SharedPreferences preferences;
 
@@ -20,53 +35,55 @@ public class Estudio extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_estudio);
-
+        preferences = getSharedPreferences("preferences", Context.MODE_PRIVATE);
         initPreguntas();
     }
 
     private void initPreguntas(){
-        //De prueba
-        preguntas.add("¿Cómo estas?");
-        respuestas.add("Bien");
-        preguntas.add("¿Cómo estas?");
-        respuestas.add("Bien1");
-        preguntas.add("¿Cómo estas?");
-        respuestas.add("Bien");
-        preguntas.add("¿Cómo estas?");
-        respuestas.add("Bien");
-        preguntas.add("¿Cómo estas?");
-        respuestas.add("Bien");
-        preguntas.add("¿Cómo estas?");
-        respuestas.add("Bien");
-        preguntas.add("¿Cómo estas?");
-        respuestas.add("Bien");
-        preguntas.add("¿Cómo estas?");
-        respuestas.add("Bien");
-        preguntas.add("¿Cómo estas?");
-        respuestas.add("Bien");
-        preguntas.add("¿Cómo estas?");
-        respuestas.add("Bien");
-        preguntas.add("¿Cómo estas?");
-        respuestas.add("Bien");
-        preguntas.add("¿Cómo estas?");
-        respuestas.add("Bien");
-        preguntas.add("¿Cómo estas?");
-        respuestas.add("Bien");
-        preguntas.add("¿Cómo estas?");
-        respuestas.add("Bien");
-        preguntas.add("¿Cómo estas?");
-        respuestas.add("Bien");
-        preguntas.add("¿Cómo estas?");
-        respuestas.add("Bien");
-        preguntas.add("¿Cómo estas?");
-        respuestas.add("Bien");
 
-        initRecyclerView();
+        String dif = "";
+        int numPreguntas = 50;
+
+
+        switch (preferences.getInt("Dif", 0)) {
+            case 0:
+                dif = "easy";
+                break;
+            case 1:
+                dif = "medium";
+                break;
+            case 2:
+                dif = "hard";
+                break;
+        }
+        ServiceAPi.getInstance().normal(numPreguntas, dif, "multiple").enqueue(new Callback<Pregunta>() {
+            @Override
+            public void onResponse(Call<Pregunta> call, Response<Pregunta> response) {
+                Pregunta pregunta = response.body();
+                List<Result> respuestas = pregunta.getResults();
+
+                for(int i=0; i<numPreguntas;i++){
+                    preguntas.add(Jsoup.parse(respuestas.get(i).getQuestion()).text());
+                    respuestasR.add(Jsoup.parse(respuestas.get(i).getCorrectAnswer()).text());
+                }
+
+                initRecyclerView();
+            }
+
+            @Override
+            public void onFailure(Call<Pregunta> call, Throwable t) {
+                Log.e("error", t.toString());
+            }
+
+        });
+
+
+
     }
 
     private void initRecyclerView(){
         RecyclerView recyclerView = findViewById(R.id.recycler_view);
-        RecyclerViewAdapter adapter = new RecyclerViewAdapter(preguntas,respuestas);
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(preguntas, respuestasR);
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
