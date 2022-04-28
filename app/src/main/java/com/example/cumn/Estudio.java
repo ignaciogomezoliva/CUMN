b4ab7f5f54845a9e058746a43cd2e0bdcf65d6d1
ignacio <ignacio.gomez.oliva@alumnos.upm.es>
2022-04-28 19:07:18 +0200

    Añadida función de traducción al modo estudio.

--- app/src/main/java/com/example/cumn/Estudio.java
+++ app/src/main/java/com/example/cumn/Estudio.java
@@ -7,10 +7,13 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
 import com.example.cumn.rest.ServiceAPi;
+import com.example.cumn.rest.TranslateAPI;
 import com.example.cumn.rest.models.Pregunta;
 import com.example.cumn.rest.models.Result;
 
@@ -21,6 +24,7 @@ import java.util.List;
 import java.util.Random;
 
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.functions.Function;
 import io.reactivex.rxjava3.schedulers.Schedulers;
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -31,22 +35,19 @@ public class Estudio extends AppCompatActivity {
     private ArrayList<String> preguntas = new ArrayList<>();
     private ArrayList<String> respuestasR = new ArrayList<>();
 
+    List<Result> respuestas = new ArrayList<>();
+
     private SharedPreferences preferences;
+
+    String dif = "";
+
+    int numPreguntas = 50;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_estudio);
         preferences = getSharedPreferences("preferences", Context.MODE_PRIVATE);
-        initPreguntas();
-    }
-
-    private void initPreguntas(){
-
-        String dif = "";
-        int numPreguntas = 50;
-
-
         switch (preferences.getInt("Dif", 0)) {
             case 0:
                 dif = "easy";
@@ -58,24 +59,90 @@ public class Estudio extends AppCompatActivity {
                 dif = "hard";
                 break;
         }
+
+        if(preferences.getInt("Lang", 0) == 0)
+            cargarPreguntasESP();
+        else
+            initPreguntas();
+    }
+
+    private void initPreguntas(){
         ServiceAPi.getInstance()
                 .normal(numPreguntas, dif, "multiple")
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(x -> {
-                    List<Result> respuestas = x.getResults();
-
-                    for(int i=0; i<numPreguntas;i++){
-                        preguntas.add(Jsoup.parse(respuestas.get(i).getQuestion()).text());
-                        respuestasR.add(Jsoup.parse(respuestas.get(i).getCorrectAnswer()).text());
-                    }
-
-                    initRecyclerView();
+                    respuestas = x.getResults();
                 });
+
+        new CountDownTimer(6000, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+            }
+
+            public void onFinish() {
+                ((TextView)findViewById(R.id.Cargando)).setVisibility(View.GONE);
+                initRecyclerView();
+            }
+        }.start();
+
+    }
+
+    public void cargarPreguntasESP(){
+        System.out.println("Tengo que traducir");
+        ServiceAPi.getInstance()
+                .normal(numPreguntas, dif, "multiple")
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .map(new Function<Pregunta, Pregunta>() {
+                    @Override
+                    public Pregunta apply (Pregunta pregunta) throws Exception {
+                        for(Result r:pregunta.getResults()){
+                            TranslateAPI.getInstance()
+                                    .traduccion("18a62192-f4c7-6641-85c8-99f2845baf26:fx", r.getQuestion(), "ES")
+                                    .subscribe(x ->{
+                                        r.setQuestion(x.getTranslations().get(0).getText());
+                                    });
+
+                            TranslateAPI.getInstance()
+                                    .traduccion("18a62192-f4c7-6641-85c8-99f2845baf26:fx", r.getCorrectAnswer(), "ES")
+                                    .subscribe(x ->{
+                                        r.setCorrectAnswer(x.getTranslations().get(0).getText());
+                                    });
+
+                        }
+
+                        return pregunta;
+                    }
+                })
+                .subscribe(x ->{
+
+                    respuestas = x.getResults();
+                    System.out.println(respuestas.get(0).getQuestion());
+
+                });
+
+
+
+        new CountDownTimer(6000, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+            }
+
+            public void onFinish() {
+                ((TextView)findViewById(R.id.Cargando)).setVisibility(View.GONE);
+                initRecyclerView();
+            }
+        }.start();
+
 
     }
 
     private void initRecyclerView(){
+        for(int i=0; i<numPreguntas;i++){
+            preguntas.add(Jsoup.parse(respuestas.get(i).getQuestion()).text());
+            respuestasR.add(Jsoup.parse(respuestas.get(i).getCorrectAnswer()).text());
+        }
         RecyclerView recyclerView = findViewById(R.id.recycler_view);
         RecyclerViewAdapter adapter = new RecyclerViewAdapter(preguntas, respuestasR);
         recyclerView.setAdapter(adapter);
