c62cce95d896a216d8f517aa061e2cdb13d32a38
YamelFonseca <yamel.fonsecac@alumnos.upm.es>
2022-05-02 20:22:30 +0200

    Added changeActivity to LogOutActivity

--- app/src/main/java/com/example/cumn/LogOutActivity.java
+++ app/src/main/java/com/example/cumn/LogOutActivity.java
@@ -1,14 +1,23 @@
 package com.example.cumn;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class LogOutActivity extends AppCompatActivity {
+    private SharedPreferences preferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_out);
+        changeActivity();
+    }
+
+    private void changeActivity() {
+            Intent intent = new Intent(this, RegisterActivity.class);
+            startActivity(intent);
     }
 }
