9aca850bfd0bd3e9e1bf346cf1ea0685f5d3b4ab
FernandoPeraltaAlba <fernando.peralta.alba@alumnos.upm.es>
2022-05-05 19:24:50 +0200

    User database with Firestore added

--- app/src/main/java/com/example/cumn/RegisterActivity.java
+++ app/src/main/java/com/example/cumn/RegisterActivity.java
@@ -3,6 +3,7 @@ package com.example.cumn;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,18 +13,25 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class RegisterActivity extends AppCompatActivity {
+    public static final String TAG = "TAG";
     private Button loginButton;
     private Button signUpButton;
     private String username;
     private String email;
     private String password;
     FirebaseAuth fAuth;
-    FirebaseFirestore fStore;;
+    FirebaseFirestore fStore;
+    String userID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +40,7 @@ public class RegisterActivity extends AppCompatActivity {
         loginButton = findViewById(R.id.loginButtonR);
         signUpButton = findViewById(R.id.signUpButton);
         fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
         setUp();
     }
 
@@ -63,6 +72,17 @@ public class RegisterActivity extends AppCompatActivity {
                 fAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener() {
                     public void onComplete(@NonNull Task task) {
                         if(task.isSuccessful()){
+                            userID = fAuth.getCurrentUser().getUid();
+                            DocumentReference documentReference = fStore.collection("users").document(userID);
+                            Map<String,Object> user = new HashMap<>();
+                            user.put("username", username);
+                            user.put("email", email);
+                            documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void unused) {
+                                    Log.d(TAG,"onSuccess: user Profile created for"+ userID);
+                                }
+                            });
                             changeActivity(true);
                         } else {
                             System.out.println(task.getException());
