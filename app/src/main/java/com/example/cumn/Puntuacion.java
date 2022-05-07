f08fa8e600f1b67afc735928604aec2c1e361edd
YamelFonseca <yamel.fonsecac@alumnos.upm.es>
2022-05-07 13:13:53 +0200

    Added Puntuacion to Firebase

--- app/src/main/java/com/example/cumn/Puntuacion.java
+++ app/src/main/java/com/example/cumn/Puntuacion.java
@@ -4,26 +4,36 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Puntuacion extends AppCompatActivity {
-
+    public static final String TAG = "TAG";
     private SharedPreferences preferences;
+    FirebaseAuth fAuth;
     FirebaseFirestore fStore;
+    String userID;
+    String scoreId;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_puntuacion);
         preferences = getSharedPreferences("preferences", Context.MODE_PRIVATE);
+        fAuth = FirebaseAuth.getInstance();
         fStore = FirebaseFirestore.getInstance();
+
 
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
@@ -68,14 +78,23 @@ public class Puntuacion extends AppCompatActivity {
             }
             int value = extras.getInt("punt");
 
-            //Obtener DocumentRefererence
+            userID = fAuth.getCurrentUser().getUid();
+            scoreId = LocalDateTime.now().toString();
+            DocumentReference documentReference = fStore
+                    .collection("users").document(userID)
+                    .collection("score").document(scoreId);
 
             Map<String,Object> score = new HashMap<>();
             score.put("category",cat);
             score.put("difficulty",dif);
             score.put("score", value);
+            documentReference.set(score).addOnSuccessListener(new OnSuccessListener<Void>() {
+                @Override
+                public void onSuccess(Void unused) {
+                    Log.d(TAG,"onSuccess: score " + scoreId + " added for "+ userID);
+                }
+            });
 
-            //Insertar la score
             ((TextView)findViewById(R.id.pts)).setText(String.valueOf(value));
         }
     }
